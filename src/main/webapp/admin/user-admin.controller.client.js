68d585358150737bcc85452c5d90bac9ee4fe5a3
bdourdoufis <dourdoufis.br@northeastern.edu>
2021-02-07 16:46:44 -0500

    Create user functionality now works properly, removed placeholder user from admin template. Working on adding user id's to remove/edit icons

--- src/main/webapp/admin/user-admin.controller.client.js
+++ src/main/webapp/admin/user-admin.controller.client.js
@@ -6,12 +6,80 @@
     var userService = new AdminUserServiceClient();
     $(main);
 
-    function main() { … }
-    function createUser() { … }
-    function deleteUser() { … }
-    function selectUser() { … }
-    function updateUser() { … }
-    function renderUsers(users) { … }
-    function findAllUsers() { … } // optional - might not need this
-    function findUserById() { … } // optional - might not need this
+    var users = [];
+
+    function main() {
+        $userRowTemplate = $('.wbdv-template')
+        $createBtn = $('.wbdv-create')
+        $tbody = $('.wbdv-tbody')
+        $usernameFld = $('#usernameFld')
+        $passwordFld = $('#passwordFld')
+        $firstNameFld = $('#firstNameFld')
+        $lastNameFld = $('#lastNameFld')
+        $roleFld = $('#roleFld')
+
+        $createBtn.click(createUser)
+
+        userService
+            .findAllUsers()
+            .then(function (usersResponse) {
+                users = usersResponse
+                renderUsers(users)
+            })
+    }
+    function createUser() {
+        debugger;
+        var username = $usernameFld.val()
+        var password = $passwordFld.val()
+        var firstName = $firstNameFld.val()
+        var lastName = $lastNameFld.val()
+        var role = $roleFld.val()
+
+        var user = {
+            username: username,
+            password: password,
+            firstName: firstName,
+            lastName: lastName,
+            role: role
+        }
+
+        userService
+            .createUser(user)
+            .then(function (userResponse) {
+                users.push(userResponse)
+                renderUsers(users)
+            })
+    }
+    function deleteUser(event) {
+        debugger;
+        var button = $(event.target)
+        var id = button.attr("id")
+        userService.deleteUser(id)
+        userService.findAllUsers()
+            .then(function (usersResponse) {
+                users = usersResponse
+                renderUsers(users)
+            })
+    }
+    function selectUser() {}
+    //function updateUser() { … }
+    function renderUsers(users) {
+        $tbody.empty()
+        for(var u in users) {
+            const user = users[u]
+            const rowClone = $userRowTemplate.clone();
+            rowClone.removeClass('wbdv-hidden');
+            rowClone.find('.wbdv-username').html(user.username);
+            rowClone.find('.wbdv-password').html(user.password);
+            rowClone.find('.wbdv-first-name').html(user.firstName);
+            rowClone.find('.wbdv-last-name').html(user.lastName);
+            rowClone.find('.wbdv-role').html(user.role);
+            rowClone.find('wbdv-remove').attr('id')
+            $tbody.append(rowClone);
+        }
+        $('.wbdv-remove').click(deleteUser)
+        $('.wbdv-edit').click(selectUser)
+    }
+    //function findAllUsers() { … } // optional - might not need this
+    //function findUserById() { … } // optional - might not need this
 })();
