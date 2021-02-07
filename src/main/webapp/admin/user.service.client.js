c37f570f4ee828ec82e58bde001e7c814339b2fa
bdourdoufis <dourdoufis.br@northeastern.edu>
2021-02-07 13:55:08 -0500

    Added initial untested user service function implementations

--- src/main/webapp/admin/user.service.client.js
+++ src/main/webapp/admin/user.service.client.js
@@ -6,9 +6,44 @@ function AdminUserServiceClient() {
     this.updateUser = updateUser;
     this.url = 'https://wbdv-generic-server.herokuapp.com/api/001642694/users';
     var self = this;
-    function createUser(user) { … }
-    function findAllUsers() { … }
-    function findUserById(userId) { … }
-    function updateUser(userId, user) { … }
-    function deleteUser(userId) { … }
+    function createUser(user) {
+        return fetch(this.url, {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(function (response) {
+            return response.json()
+        })
+    }
+    function findAllUsers() {
+        return fetch(this.url).then(function(response) {
+            return response.json()
+        })
+    }
+
+    function findUserById(userId) {
+        return fetch(this.url + '/' + userId).then(function(response) {
+            return response.json()
+        })
+    }
+    function updateUser(userId, user) {
+        return fetch(this.url + '/' + userId, {
+            method: 'PUT',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(function (response) {
+            return response.json()
+        })
+    }
+    function deleteUser(userId) {
+        return fetch(this.url + '/' + userId, {
+            method: 'DELETE',
+        }).then(function (response) {
+            return response.json()
+        })
+    }
 }
