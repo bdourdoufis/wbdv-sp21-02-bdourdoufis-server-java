2c7c8861cfc444f8ca01160179d49d8b747ba24b
bdourdoufis <dourdoufis.br@northeastern.edu>
2021-02-07 22:44:02 -0500

    Updated admin directory name to user-admin, added initial implementation of user update functionality

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,7 +1,7 @@
 (function () {
     var $usernameFld, $passwordFld;
     var $firstNameFld, $lastNameFld, $roleFld;
-    var $removeBtn, $editBtn, $createBtn;
+    var $removeBtn, $editBtn, $createBtn, $updateBtn;
     var $userRowTemplate, $tbody;
     var userService = new AdminUserServiceClient();
     $(main);
@@ -17,8 +17,10 @@
         $firstNameFld = $('#firstNameFld')
         $lastNameFld = $('#lastNameFld')
         $roleFld = $('#roleFld')
+        $updateBtn = $('.wbdv-update')
 
         $createBtn.click(createUser)
+        $updateBtn.click(updateUser)
 
         userService
             .findAllUsers()
@@ -28,7 +30,6 @@
             })
     }
     function createUser() {
-        debugger;
         var username = $usernameFld.val()
         var password = $passwordFld.val()
         var firstName = $firstNameFld.val()
@@ -43,6 +44,11 @@
             role: role
         }
 
+        $usernameFld.val("")
+        $passwordFld.val("")
+        $firstNameFld.val("")
+        $lastNameFld.val("")
+
         userService
             .createUser(user)
             .then(function (userResponse) {
@@ -51,7 +57,6 @@
             })
     }
     function deleteUser(event) {
-        debugger;
         var button = $(event.target)
         var id = button.attr("id")
         userService.deleteUser(id)
@@ -63,8 +68,52 @@
                     })
             })
     }
-    function selectUser() {}
-    //function updateUser() { … }
+    function selectUser(event) {
+        debugger;
+        var button = $(event.target)
+        var id = button.attr("id")
+        userService.findUserById(id)
+            .then(function (selectedUser) {
+                $usernameFld.val(selectedUser.username)
+                $passwordFld.val(selectedUser.password)
+                $firstNameFld.val(selectedUser.firstName)
+                $lastNameFld.val(selectedUser.lastName)
+                $roleFld.val(selectedUser.role)
+                $('.wbdv-update').attr('id', selectedUser._id)
+            })
+    }
+    function updateUser() {
+        if ($tbody.find('.wbdv-update').attr('id') != -1) {
+            var username = $usernameFld.val()
+            var password = $passwordFld.val()
+            var firstName = $firstNameFld.val()
+            var lastName = $lastNameFld.val()
+            var role = $roleFld.val()
+
+            var user = {
+                username: username,
+                password: password,
+                firstName: firstName,
+                lastName: lastName,
+                role: role
+            }
+
+            $usernameFld.val("")
+            $passwordFld.val("")
+            $firstNameFld.val("")
+            $lastNameFld.val("")
+            debugger;
+
+            userService.updateUser($('.wbdv-update').attr('id'), user)
+                .then(function (response) {
+                    userService.findAllUsers()
+                        .then(function (usersResponse) {
+                            users = usersResponse
+                            renderUsers(users)
+                        })
+                })
+        }
+    }
     function renderUsers(users) {
         $tbody.empty()
         for(var i = 0; i < users.length; i++) {
@@ -77,11 +126,10 @@
             rowClone.find('.wbdv-last-name').html(user.lastName);
             rowClone.find('.wbdv-role').html(user.role);
             rowClone.find('.wbdv-remove').attr('id', user._id)
+            rowClone.find('.wbdv-edit').attr('id', user._id)
             $tbody.append(rowClone);
         }
         $('.wbdv-remove').click(deleteUser)
         $('.wbdv-edit').click(selectUser)
     }
-    //function findAllUsers() { … } // optional - might not need this
-    //function findUserById() { … } // optional - might not need this
 })();
